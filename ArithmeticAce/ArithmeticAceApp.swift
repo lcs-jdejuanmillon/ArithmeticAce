e2452915a2a5235ac7c61788d3ef07221ab9de53
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-02-07 08:51:46 -0500

    Initial Commit

---
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticAceApp.swift
+//  ArithmeticAce
+//
+//  Created by Jacobo de Juan Millon on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
