fb45a37dfc9ac129b89f0d11ad1f84c2e2cf6b86
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-02-07 09:25:26 -0500

    Have part of static done

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -11,35 +11,31 @@ struct ContentView: View {
     var body: some View {
         ScrollView() {
             VStack() {
-            HStack {
-                Text("×")
-                    .font(.system(size: 72))
-                Spacer()
-                VStack {
-                    Text("5")
-                        .font(.system(size: 72))
-                    Text("6")
-                        .font(.system(size: 72))
+                HStack {
+                    Text("×")
+                    Spacer()
+                    VStack {
+                        Text("5")
+                        Text("6")
+                    }
                 }
-                
-            }
-            Divider()
-            HStack() {
-                Spacer()
-                Text("30")
-                    .font(.system(size: 72))
-            }
-            Button(action: {
-                // NOTE: Output will not be shown unless this app is run in the "full" simulator
-                print("Button was pressed")
-            }, label: {
-                Text("New question")
-                    .font(.system(size: 36))
-            })
+                Divider()
+                HStack() {
+                    Spacer()
+                    Text("30")
+                }
+                Button(action: {
+                    // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                    print("Button was pressed")
+                }, label: {
+                    Text("New question")
+                        .font(.system(size: 36))
+                })
                     .buttonStyle(.bordered)
-        }
+            }
+            .font(.system(size: 72))
             .padding()
-    }
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
